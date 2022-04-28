de08905716e50100d2a4607248823706944fd50f
Srilekhaboda <97424801+Srilekhaboda@users.noreply.github.com>
2022-04-28 09:28:00 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],s,c;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             s++;         }         else         {             c++;         }     }     printf("%d %d",s,c); }

---
+++ Count.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],s,c;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            s++;
+        }
+        else
+        {
+            c++;
+        }
+    }
+    printf("%d %d",s,c);
+}
