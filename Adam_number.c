6a2955cab56e19a6b61341928250bf60e0399da7
Srilekhaboda <97424801+Srilekhaboda@users.noreply.github.com>
2022-06-22 21:01:31 +0530

    #include<stdio.h> int reverse(int n) {     int d,rev=0;     while(n!=0)     {         d=n%10;         rev=rev*10+d;         n=n/10;     }     return rev; } int main() {     int n,r,s,m,k;     scanf("%d",&n);     s=n*n;     r=reverse(n);     m=r*r;     k=reverse(m);     if(s==k)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Adam_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int reverse(int n)
+{
+    int d,rev=0;
+    while(n!=0)
+    {
+        d=n%10;
+        rev=rev*10+d;
+        n=n/10;
+    }
+    return rev;
+}
+int main()
+{
+    int n,r,s,m,k;
+    scanf("%d",&n);
+    s=n*n;
+    r=reverse(n);
+    m=r*r;
+    k=reverse(m);
+    if(s==k)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
