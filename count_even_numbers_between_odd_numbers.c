779ea5aa883cc56434aad3921fbca6f56f3cad80
Srilekhaboda <97424801+Srilekhaboda@users.noreply.github.com>
2022-06-29 21:43:28 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<n;i++)     {         if(arr[i]%2==0)         {             if(arr[i-1]%2!=0 && arr[i+1]%2!=0)             {                 if(i!=n-1)                 {                     c++;                 }             }         }     }     printf("%d",c);     return 0; }

---
+++ count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            if(arr[i-1]%2!=0 && arr[i+1]%2!=0)
+            {
+                if(i!=n-1)
+                {
+                    c++;
+                }
+            }
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
