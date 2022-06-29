53e87919a3119572258c968e8e865b9e58bba95a
Srilekhaboda <97424801+Srilekhaboda@users.noreply.github.com>
2022-06-29 21:49:40 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0 && arr[i]%2==1)         {             c++;         }     }     if(c>0)     {         printf("False");     }     else     {         printf("True");     } }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,x[100],c=0;
+    int arr[100],n,i,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&x[i]);
+        scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i+=2)
+    for(i=0;i<n;i++)
     {
-        if(x[i]%2!=0)
+        if(i%2==0 && arr[i]%2==1)
         {
-            printf("False");
             c++;
-            break;
         }
     }
-    if(c==0)
-    printf("True");
+    if(c>0)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
 }
