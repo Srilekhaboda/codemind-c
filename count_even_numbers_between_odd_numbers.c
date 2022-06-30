b98d8b797c109e64dcd96e16fa8f6662b033ae4d
Srilekhaboda <97424801+Srilekhaboda@users.noreply.github.com>
2022-06-30 22:32:24 +0530

    #include<stdio.h> int even_count(int *arr,int n) {     int i,k=0;     for(i=1;i<n-1;i++)     {         if(arr[i-1]%2!=0 && arr[i+1]%2!=0)         {             if(arr[i]%2==0)             {                 k++;             }         }     }     return k; } int main() {     int n,i,arr[100],k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     k=even_count(arr,n);     printf("%d ",k); }

--- count_even_numbers_between_odd_numbers.c
+++ count_even_numbers_between_odd_numbers.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
+int even_count(int *arr,int n)
+{
+    int i,k=0;
+    for(i=1;i<n-1;i++)
+    {
+        if(arr[i-1]%2!=0 && arr[i+1]%2!=0)
+        {
+            if(arr[i]%2==0)
+            {
+                k++;
+            }
+        }
+    }
+    return k;
+}
 int main()
 {
-    int arr[100],n,i,c=0;
+    int n,i,arr[100],k;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=1;i<n;i++)
-    {
-        if(arr[i]%2==0)
-        {
-            if(arr[i-1]%2!=0 && arr[i+1]%2!=0)
-            {
-                if(i!=n-1)
-                {
-                    c++;
-                }
-            }
-        }
-    }
-    printf("%d",c);
-    return 0;
+    k=even_count(arr,n);
+    printf("%d ",k);
 }
